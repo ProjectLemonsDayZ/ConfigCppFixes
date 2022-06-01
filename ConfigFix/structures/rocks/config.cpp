3edf4f6a010d7aa607eeadf86b0c7b791b681274
ProjectLemonsDayZ <106694777+ProjectLemonsDayZ@users.noreply.github.com>
2022-06-01 13:09:07 -0400

    Updated To Feedback Tracker Report
    https://feedback.bistudio.com/T165558

---
+++ ConfigFix/structures/rocks/config.cpp
@@ -0,0 +1,197 @@
+class CfgPatches
+{
+	class DZ_Rocks
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"DZ_Data"
+		};
+	};
+};
+class CfgVehicles // Changed "CfgNonAIVehicles" to "CfgVehicles" //
+{
+	class StaticObject; // Changed "StaticObject" to "HouseNoDestruct" //
+	class Static_stone1: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\stone1.p3d";
+	};
+	class Static_stone2: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\stone2.p3d";
+	};
+	class Static_stone3: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\stone3.p3d";
+	};
+	class Static_stone4: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\stone4.p3d";
+	};
+	class Static_stone5: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\stone5.p3d";
+	};
+	class Static_rock_apart1: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_apart1.p3d";
+	};
+	class Static_rock_apart2: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_apart2.p3d";
+	};
+	class Static_rock_bright_apart1: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_bright_apart1.p3d";
+	};
+	class Static_rock_bright_apart2: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_bright_apart2.p3d";
+	};
+	class Static_rock_bright_monolith1: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_bright_monolith1.p3d";
+	};
+	class Static_rock_bright_monolith2: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_bright_monolith2.p3d";
+	};
+	class Static_rock_bright_monolith3: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_bright_monolith3.p3d";
+	};
+	class Static_rock_bright_monolith4: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_bright_monolith4.p3d";
+	};
+	class Static_rock_bright_spike1: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_bright_spike1.p3d";
+	};
+	class Static_rock_bright_spike2: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_bright_spike2.p3d";
+	};
+	class Static_rock_bright_spike3: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_bright_spike3.p3d";
+	};
+	class Static_rock_bright_wallh1: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_bright_wallh1.p3d";
+	};
+	class Static_rock_bright_wallh2: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_bright_wallh2.p3d";
+	};
+	class Static_rock_bright_wallh3: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_bright_wallh3.p3d";
+	};
+	class Static_rock_bright_wallv: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_bright_wallv.p3d";
+	};
+	class Static_rock_monolith1: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_monolith1.p3d";
+	};
+	class Static_rock_monolith2: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_monolith2.p3d";
+	};
+	class Static_rock_monolith3: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_monolith3.p3d";
+	};
+	class Static_rock_monolith4: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_monolith4.p3d";
+	};
+	class Static_rock_spike1: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_spike1.p3d";
+	};
+	class Static_rock_spike2: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_spike2.p3d";
+	};
+	class Static_rock_spike3: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_spike3.p3d";
+	};
+	class Static_rock_wallh1: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_wallh1.p3d";
+	};
+	class Static_rock_wallh2: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_wallh2.p3d";
+	};
+	class Static_rock_wallh3: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_wallh3.p3d";
+	};
+	class Static_rock_wallv: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\rock_wallv.p3d";
+	};
+	class Static_stone5_trail_b: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\stone5_trail_b.p3d";
+	};
+	class Static_stone5_trail_g: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\stone5_trail_g.p3d";
+	};
+	class Static_stone5_trail_r: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\stone5_trail_r.p3d";
+	};
+	class Static_stone5_trail_y: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\stone5_trail_y.p3d";
+	};
+	class Static_stones_erosion: HouseNoDestruct // Created New Class //
+	{
+		scope=2;
+		model="DZ\rocks\stones_erosion.p3d";
+	};
+};
